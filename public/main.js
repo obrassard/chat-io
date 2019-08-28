d687301de375904e5568cd2d8ad5a7feb99fe50e
Olivier Brassard <brassard.oli@gmail.com>
2019-08-28 13:59:50 -0400

    Fix localstorage

--- public/main.js
+++ public/main.js
@@ -57,7 +57,7 @@ $(function () {
         let usr = localStorage.getItem('username');
         if (!usr){
             usr = prompt('What is your name ?');
-            localStorage.setItem('username', user);
+            localStorage.setItem('username', usr);
         }
         return usr;
     }
